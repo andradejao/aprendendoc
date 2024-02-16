306af00d2c66bcb538c102b15ba4a033e2cc6a37
João Andrade <joaoandradeprime99@gmail.com>
2024-02-16 17:21:32 -0300

    Conversor binário para decimal

---
+++ while-bin-dec.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    int bin[6]={1,0,1,1,1,0};
+    int rs = 0;
+    int pos = 0;
+    int exp = 5;
+
+    while (pos <=5){
+        rs += bin[pos] * (pow (2, exp));
+        pos++;
+        exp--;
+
+    }
+    printf("%d \n", rs);
+    return 0;
+    
+
+
+
+    
+    
+  
+    return 0;
+
+    }
